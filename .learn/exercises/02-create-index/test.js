4ded9d076a1260e7f4a3b1b8fe717203f100d84c
brunasso <53584556+brunasso@users.noreply.github.com>
2024-07-09 00:39:29 +0000

    Proyecto terminado

--- .learn/exercises/02-create-index/test.js
+++ .learn/exercises/02-create-index/test.js
@@ -2,5 +2,6 @@ const fs = require("fs");
 
 test("Make sure you have your index.html", function(){
     const indexExists = fs.existsSync(`./index.html`);
+    console.log(fs.existsSync);
     expect(indexExists).toBe(true);
 })
